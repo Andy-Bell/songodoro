3cc87ed86f869e506d91c0c4d5e78607f6c63692
Matt Butler <mattb-w@hotmail.co.uk>
2016-07-19 17:33:37 +0100

    adds prop function

--- components/Main.js
+++ components/Main.js
@@ -2,14 +2,19 @@ import React from 'react';
 import Timer from './Timer'
 
 export default React.createClass({
-  spotifyPlayer: () => {
-    window.alert('MUSIC TIME')
+
+  getInitialState: () => { return {spotify: <div>NO MUSIC</div>}
   },
+  renderPlayer: function () {
+    this.setState({spotify: <div>MUSIC TIME</div>})
+  },
+
   render() {
     return (
       <div>
         <h1>{this.props.hello}</h1>
-        <Timer activatePlayer={this.spotifyPlayer}/>
+        <Timer activatePlayer={this.renderPlayer}/>
+        {this.state.spotify}
       </div>
     );
   }
