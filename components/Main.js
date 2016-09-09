bb53bf100151a76913a5d99e41044c6d46a5f6c9
Matt Butler <mattb-w@hotmail.co.uk>
2016-09-09 08:51:47 +0100

    Update Main.js

--- components/Main.js
+++ components/Main.js
@@ -41,7 +41,7 @@ export default React.createClass({
   render() {
     return (
       <div>
-        <div className="title"><span className="slash">
+        <div className="title"><span className="slash">//  </span><span className="title-name">Songodoro</span><span className="slash">  //</span></div>
         {this.state.currentTrack ? <SpotifyPlayer track={this.state.currentTrack}/>  : null }
         <Timer ref="timer" activatePlayer={this.mountPlayer} deactivatePlayer={this.unMountPlayer} displayTimer={this.state.currentTrack ? false : true }/>
       </div>
